2ab382ff0b1589ed6f35c191791e6c97f685c2a2
김세종 <gimsejong991@gmail.com>
2025-07-22 00:43:49 +0900

    푸시확인용2

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -49,10 +49,6 @@
         <nav class="mt-4">
           <ul class="pagination justify-content-center">
             <li class="page-item disabled"><a class="page-link">← Previous</a></li>
-            <li class="page-item active"><a class="page-link">1</a></li>
-            <li class="page-item"><a class="page-link">2</a></li>
-            <li class="page-item"><a class="page-link">3</a></li>
-            <li class="page-item"><a class="page-link">…</a></li>
           </ul>
         </nav>
       </div>

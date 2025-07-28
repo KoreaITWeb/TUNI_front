32681445b67edba38d40d26103205021487d386c
KSJ <gimsejong991@gmail.com>
2025-07-28 15:30:43 +0900

    포트번호 수정

--- src/main.js
+++ src/main.js
@@ -6,5 +6,5 @@ import '@/assets/styles/global/layout.css';
 import axios from 'axios'
 
 createApp(App).use(router).use(router).use(router).mount('#app')
-axios.defaults.baseURL = 'http://localhost:8441'  // 백엔드 서버 주소
+axios.defaults.baseURL = 'http://localhost:8443'  // 백엔드 서버 주소
 axios.defaults.withCredentials = true 

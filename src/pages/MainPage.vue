49a379598f2d0621a4a8c02337938431c50ddccf
aaronshin43 <aaronshin.exe@gmail.com>
2025-08-05 19:59:36 +0900

    오류 수정

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -95,7 +95,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, reactive } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 import api from '@/api';
